baf5465c0e366aa4175ee1ffa9df8516a6020129
thangndgit <81653169+thangndgit@users.noreply.github.com>
2023-04-21 16:01:40 +0700

    upload lab04

---
+++ Lab4.2/src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => res.sendFile("views/index.html", { root: __dirname }));
+app.get("/users", (req, res) => res.sendFile("views/users.html", { root: __dirname }));
+
+app.listen(port, () => console.log("Server is running on port " + port));
